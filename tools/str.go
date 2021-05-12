afee5f0b8aac5a3b41808f322b82e7a27e0d1e77
lvxin <lvxin0315@163.com>
2021-05-12 10:04:33 +0800

    设置默认值

--- tools/str.go
+++ tools/str.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// StrCut
 /**
  * @Author lvxin0315@163.com
  * @Description 截取字符串
@@ -26,6 +27,7 @@ func StrCut(str string, start, cutLen int) string {
 	return string(rs[start:cutLen])
 }
 
+// FilterChinese
 /**
  * @Author lvxin0315@163.com
  * @Description 过滤掉中文
@@ -41,4 +43,49 @@ func FilterChinese(src string) string {
 		rArr = append(rArr, strings.Join(v, ""))
 	}
 	return strings.Join(rArr, "")
+}
+
+// CamelString camel string, xx_yy to XxYy
+func CamelString(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == '_' && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
+
+// Capitalize 如果是小写字母, 则变换为大写字母
+func Capitalize(str string) string {
+	var upperStr string
+	vv := []rune(str) // 后文有介绍
+	for i := 0; i < len(vv); i++ {
+		if i == 0 {
+			if vv[i] >= 97 && vv[i] <= 122 { // 后文有介绍
+				vv[i] -= 32 // string的码表相差32位
+				upperStr += string(vv[i])
+			} else {
+				return str
+			}
+		} else {
+			upperStr += string(vv[i])
+		}
+	}
+	return upperStr
+
 }
